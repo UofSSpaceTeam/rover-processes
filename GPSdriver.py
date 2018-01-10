dec11708b5bfb2acc6c6b0fe000768b496126aba
Carl Hofmeister <hofmeister.carl@gmail.com>
2018-01-09 21:25:09 -0600

    Expanded all the tabs

--- GPSdriver.py
+++ GPSdriver.py
@@ -25,45 +25,45 @@ SERIAL = 'COMM1'
 BAUD = 115200
 
 class GPSPosition:
-	# Earth's radius in metres
+    # Earth's radius in metres
     RADIUS = 6371008.8
 
-	def __init__(self, lat, lon, mode=0):
-		# lat an lon are assumed to be in radians
-		self.lat = lat
-		self.lon = lon
-		self.mode = mode # 0=SPP, 1=Float RTK, 2=Fixed RTK
+    def __init__(self, lat, lon, mode=0):
+        # lat an lon are assumed to be in radians
+        self.lat = lat
+        self.lon = lon
+        self.mode = mode # 0=SPP, 1=Float RTK, 2=Fixed RTK
 
-	def distance(self, them):
-		''' Returns the distance to another GPSPositions on earth'''
-		hav = lambda z: (1 - cos(z)) / 2   # haversine
-		ahav = lambda z: 2 * asin(sqrt(z)) # inverse haversine
+    def distance(self, them):
+        ''' Returns the distance to another GPSPositions on earth'''
+        hav = lambda z: (1 - cos(z)) / 2   # haversine
+        ahav = lambda z: 2 * asin(sqrt(z)) # inverse haversine
 
-		d_lat = them.lat - self.lat
-		d_lon = them.lon - self.lon
+        d_lat = them.lat - self.lat
+        d_lon = them.lon - self.lon
 
-		z = (hav(d_lat) + cos(self.lat) * cos(them.lat) * hav(d_lon))
+        z = (hav(d_lat) + cos(self.lat) * cos(them.lat) * hav(d_lon))
 
-		return GPSPosition.RADIUS * ahav(z)
+        return GPSPosition.RADIUS * ahav(z)
 
-	def bearing(self, them):
-		''' Returns the bearing to another GPSPositions on earth'''
-		d_lat = them.lat - self.lat
-		d_lon = them.lon - self.lon
+    def bearing(self, them):
+        ''' Returns the bearing to another GPSPositions on earth'''
+        d_lat = them.lat - self.lat
+        d_lon = them.lon - self.lon
 
-		y = sin(d_lon) * cos(them.lat)
-		x = (cos(self.lat) * sin(them.lat)
-						- sin(self.lat) * cos(them.lat) * cos(d_lat))
+        y = sin(d_lon) * cos(them.lat)
+        x = (cos(self.lat) * sin(them.lat)
+                        - sin(self.lat) * cos(them.lat) * cos(d_lat))
 
-		return math.degrees(atan2(y, x))
+        return math.degrees(atan2(y, x))
 
-	def gpsPosition(self, bearing, distance):
-		r_lat = math.radians(self.lat)
-		r_lon = math.radians(self.lon)
-		target_lat = asin(sin(r_lat)*cos(distance/GPSPosition.RADIUS) +cos(r_lat)*sin(distance/GPSPosition.RADIUS)*cos(math.radians(bearing)))
+    def gpsPosition(self, bearing, distance):
+        r_lat = math.radians(self.lat)
+        r_lon = math.radians(self.lon)
+        target_lat = asin(sin(r_lat)*cos(distance/GPSPosition.RADIUS) +cos(r_lat)*sin(distance/GPSPosition.RADIUS)*cos(math.radians(bearing)))
 
-		target_lon = r_lon + atan2(sin(bearing)*sin(distance/GPSPosition.RADIUS)*cos(r_lat),cos(distance/GPSPosition.RADIUS)-sin(r_lat)*sin(target_lat))
-		return GPSPosition(math.degrees(target_lat), math.degrees(target_lon))
+        target_lon = r_lon + atan2(sin(bearing)*sin(distance/GPSPosition.RADIUS)*cos(r_lat),cos(distance/GPSPosition.RADIUS)-sin(r_lat)*sin(target_lat))
+        return GPSPosition(math.degrees(target_lat), math.degrees(target_lon))
 
 
 GPSdevice = Device('GPSdevice', 'demo-device')
--- NavigationProcess.py
+++ NavigationProcess.py
@@ -77,168 +77,168 @@ NavDevice.storage.lidar_scan_finish = False
 @NavDevice.every(NavDevice.storage.loop_delay)
 async def every():
     if NavDevice.storage.state == "waiting":
-		NavDevice.wait_state()
-	elif NavDevice.storage.state == "driving":
-		NavDevice.drive_state()
-	elif NavDevice.storage.state == "manual":
-		pass # don't do anything in manual control mode
-	else:
-		.state = "waiting"
-	NavDevice.update_wheel_velocity()
+        NavDevice.wait_state()
+    elif NavDevice.storage.state == "driving":
+        NavDevice.drive_state()
+    elif NavDevice.storage.state == "manual":
+        pass # don't do anything in manual control mode
+    else:
+        .state = "waiting"
+    NavDevice.update_wheel_velocity()
 
 async def drive_state():
-	''' Function for handling drive state '''
-	if  NavDevice.storage.position is not None:
-		distance =  NavDevice.storage.position.distance(NavDevice.storage.target)
-		bearing = NavDevice.storage.position.bearing(NavDevice.storage.target)
-		difference = bearing - NavDevice.storage.heading
-		if abs(difference) > NavDevice.storage.bearing_error:
-			if difference < 0:
-				await NavDevice.publish("DriveRotateLeft", MIN_WHEEL_RPM)
-				NavDevice.storage.left_rpm = -MIN_WHEEL_RPM
-				NavDevice.storage.right_rpm = MIN_WHEEL_RPM
-			else:
-				await NavDevice.publish("DriveRotateRight", MIN_WHEEL_RPM)
-				NavDevice.storage.left_rpm = MIN_WHEEL_RPM
-				NavDevice.storage.right_rpm = -MIN_WHEEL_RPM
-			return # back to loop
-		elif distance > NavDevice.storage.target_reached_distance:
-			await NavDevice.publish("DriveForward", MIN_WHEEL_RPM)
-			NavDevice.storage.left_rpm = MIN_WHEEL_RPM
-			NavDevice.storage.right_rpm = MIN_WHEEL_RPM
-			return #back to loop
-		else:
-			NavDevice.storage.target = None
-			await NavDevice.publish("TargetReached")
-			NavDevice.storage.state = "waiting"
+    ''' Function for handling drive state '''
+    if  NavDevice.storage.position is not None:
+        distance =  NavDevice.storage.position.distance(NavDevice.storage.target)
+        bearing = NavDevice.storage.position.bearing(NavDevice.storage.target)
+        difference = bearing - NavDevice.storage.heading
+        if abs(difference) > NavDevice.storage.bearing_error:
+            if difference < 0:
+                await NavDevice.publish("DriveRotateLeft", MIN_WHEEL_RPM)
+                NavDevice.storage.left_rpm = -MIN_WHEEL_RPM
+                NavDevice.storage.right_rpm = MIN_WHEEL_RPM
+            else:
+                await NavDevice.publish("DriveRotateRight", MIN_WHEEL_RPM)
+                NavDevice.storage.left_rpm = MIN_WHEEL_RPM
+                NavDevice.storage.right_rpm = -MIN_WHEEL_RPM
+            return # back to loop
+        elif distance > NavDevice.storage.target_reached_distance:
+            await NavDevice.publish("DriveForward", MIN_WHEEL_RPM)
+            NavDevice.storage.left_rpm = MIN_WHEEL_RPM
+            NavDevice.storage.right_rpm = MIN_WHEEL_RPM
+            return #back to loop
+        else:
+            NavDevice.storage.target = None
+            await NavDevice.publish("TargetReached")
+            NavDevice.storage.state = "waiting"
 
 async def wait_state():
-	''' Function for handling waiting state '''
-	if NavDevice.storage.autonomous_mode:
-		if len(NavDevice.storage.waypoints) == 0:
-			await NavDevice.publish("DriveStop", 0)
-	else:
-		await NavDevice.publish("DriveStop", 0)
+    ''' Function for handling waiting state '''
+    if NavDevice.storage.autonomous_mode:
+        if len(NavDevice.storage.waypoints) == 0:
+            await NavDevice.publish("DriveStop", 0)
+    else:
+        await NavDevice.publish("DriveStop", 0)
 
 def update_wheel_velocity():
-	NavDevice.storage.right_speed = NavDevice.storage.right_rpm*WHEEL_RADIUS/2
-	NavDevice.storage.left_speed = NavDevice.storage.left_rpm*WHEEL_RADIUS/2
+    NavDevice.storage.right_speed = NavDevice.storage.right_rpm*WHEEL_RADIUS/2
+    NavDevice.storage.left_speed = NavDevice.storage.left_rpm*WHEEL_RADIUS/2
 
 @NavDevice.on('CompassDataMessage')
 async def CompassDataMessage_callback('CompassDataMessage', msg):
-	''' CompassDataMessage contains:
-		heading (degrees): Relative to north, the angle of
-			rotation on the axis normal to the earth's surface.
-		pitch (degrees):
-		roll (degrees):
-	'''
-	if NavDevice.storage.heading is not None:
-		NavDevice.storage.heading_last = NavDevice.storage.heading
-		if NavDevice.storage.state == "waiting":
-			# rolling average
-			NavDevice.storage.heading = (NavDevice.storage.heading_samples*NavDevice.storage.heading + msg.heading)/(NavDevice.storage.heading_samples+1)
-			NavDevice.storage.heading_samples += 1
-		else:
-			tmp = time.time()
-			d_t = tmp-NavDevice.storage.last_compassmessage
-			NavDevice.storage.last_compassmessage = tmp
-			NavDevice.storage.heading = NavDevice.g_h_filter(msg.heading, NavDevice.storage.heading,
-					(NavDevice.storage.right_speed-NavDevice.storage.left_speed)/ROVER_WIDTH, 0.8, d_t)
-			await NavDevice.publish("RoverHeading", NavDevice.storage.heading)
-		# self.log("heading: {}".format(self.heading))
+    ''' CompassDataMessage contains:
+        heading (degrees): Relative to north, the angle of
+            rotation on the axis normal to the earth's surface.
+        pitch (degrees):
+        roll (degrees):
+    '''
+    if NavDevice.storage.heading is not None:
+        NavDevice.storage.heading_last = NavDevice.storage.heading
+        if NavDevice.storage.state == "waiting":
+            # rolling average
+            NavDevice.storage.heading = (NavDevice.storage.heading_samples*NavDevice.storage.heading + msg.heading)/(NavDevice.storage.heading_samples+1)
+            NavDevice.storage.heading_samples += 1
+        else:
+            tmp = time.time()
+            d_t = tmp-NavDevice.storage.last_compassmessage
+            NavDevice.storage.last_compassmessage = tmp
+            NavDevice.storage.heading = NavDevice.g_h_filter(msg.heading, NavDevice.storage.heading,
+                    (NavDevice.storage.right_speed-NavDevice.storage.left_speed)/ROVER_WIDTH, 0.8, d_t)
+            await NavDevice.publish("RoverHeading", NavDevice.storage.heading)
+        # self.log("heading: {}".format(self.heading))
 
 @NavDevice.on('targetGPS')
 async def targetGPS_callback('targetGPS', pos):
-	''' Targets a new GPS coordinate '''
-	target = GPSPosition(radians(pos[0]), radians(pos[1]))
-	await NavDevice.publish("TargetReached", False)
-	if target.distance(NavDevice.storage.position) <= NavDevice.storage.maximum_target_distance:
-		if len(NavDevice.storage.waypoints) > 1:
-			NavDevice.storage.waypoints.append(target)
-			NavDevice.storage.target = waypoints[0]
-		else:
-			NavDevice.storage.target = target
-		if NavDevice.storage.state != "manual":
-			NavDevice.storage.state = "driving"
+    ''' Targets a new GPS coordinate '''
+    target = GPSPosition(radians(pos[0]), radians(pos[1]))
+    await NavDevice.publish("TargetReached", False)
+    if target.distance(NavDevice.storage.position) <= NavDevice.storage.maximum_target_distance:
+        if len(NavDevice.storage.waypoints) > 1:
+            NavDevice.storage.waypoints.append(target)
+            NavDevice.storage.target = waypoints[0]
+        else:
+            NavDevice.storage.target = target
+        if NavDevice.storage.state != "manual":
+            NavDevice.storage.state = "driving"
 
 @NavDevice.on('singlePointGPS')
 async def singlePointGPS_callback('singlePointGPS', pos):
-	''' Updates GPS position '''
-	#std_dev 1.663596084712623e-05, 2.1743680968892167e-05
-	# self.log("{},{}".format(degrees(pos.lat), degrees(pos.lon)))
-	if NavDevice.storage.state == "waiting" and NavDevice.storage.position is not None:
-		lat = (NavDevice.storage.position.lat + pos.lat)/(NavDevice.storage.pos_samples)
-		lon = (NavDevice.storage.pos_samples*NavDevice.storage.position.lon + pos.lon)/(NavDevice.storage.pos_samples+1)
-		NavDevice.storage.pos_samples += 1
-		await NavDevice.publish("RoverPosition", [degrees(NavDevice.storage.position.lat), degrees(NavDevice.storage.position.lon)])
-		return
-	if NavDevice.storage.position is not None:
-		NavDevice.storage.position_last = NavDevice.storage.position
-		NavDevice.storage.position = pos
-		await NavDevice.publish("RoverPosition", [degrees(pos.lat), degrees(pos.lon)])
-	else:
-		if len(NavDevice.storage.starting_calibration_gps[0]) < CALIBRATION_SAMPLES:
-			NavDevice.storage.starting_calibration_gps[0].append(pos.lat)
-			NavDevice.storage.starting_calibration_gps[1].append(pos.lon)
-		else:
-			# Done averaging
-			NavDevice.storage.starting_calibration_gps[0].append(pos.lat)
-			NavDevice.storage.starting_calibration_gps[1].append(pos.lon)
-			NavDevice.storage.position = GPSPosition(mean(NavDevice.storage.starting_calibration_gps[0]),
-				mean(NavDevice.storage.starting_calibration_gps[1]))
-			#self.log("{},{}".format(degrees(pos.lat), degrees(pos.lon)), "INFO")
+    ''' Updates GPS position '''
+    #std_dev 1.663596084712623e-05, 2.1743680968892167e-05
+    # self.log("{},{}".format(degrees(pos.lat), degrees(pos.lon)))
+    if NavDevice.storage.state == "waiting" and NavDevice.storage.position is not None:
+        lat = (NavDevice.storage.position.lat + pos.lat)/(NavDevice.storage.pos_samples)
+        lon = (NavDevice.storage.pos_samples*NavDevice.storage.position.lon + pos.lon)/(NavDevice.storage.pos_samples+1)
+        NavDevice.storage.pos_samples += 1
+        await NavDevice.publish("RoverPosition", [degrees(NavDevice.storage.position.lat), degrees(NavDevice.storage.position.lon)])
+        return
+    if NavDevice.storage.position is not None:
+        NavDevice.storage.position_last = NavDevice.storage.position
+        NavDevice.storage.position = pos
+        await NavDevice.publish("RoverPosition", [degrees(pos.lat), degrees(pos.lon)])
+    else:
+        if len(NavDevice.storage.starting_calibration_gps[0]) < CALIBRATION_SAMPLES:
+            NavDevice.storage.starting_calibration_gps[0].append(pos.lat)
+            NavDevice.storage.starting_calibration_gps[1].append(pos.lon)
+        else:
+            # Done averaging
+            NavDevice.storage.starting_calibration_gps[0].append(pos.lat)
+            NavDevice.storage.starting_calibration_gps[1].append(pos.lon)
+            NavDevice.storage.position = GPSPosition(mean(NavDevice.storage.starting_calibration_gps[0]),
+                mean(NavDevice.storage.starting_calibration_gps[1]))
+            #self.log("{},{}".format(degrees(pos.lat), degrees(pos.lon)), "INFO")
 
 @NavDevice.on('GPSVelocity')
 def GPSVelocity_callback('GPSVelocity', vel):
-	''' Updates velocity from GPS unit '''
-	# std_dev 0.04679680341613995, 0.035958365746391524
-	# self.log("{},{}".format(vel[0], vel[1]))
-	NavDevice.storage.velocity = vel
+    ''' Updates velocity from GPS unit '''
+    # std_dev 0.04679680341613995, 0.035958365746391524
+    # self.log("{},{}".format(vel[0], vel[1]))
+    NavDevice.storage.velocity = vel
 
 
 @NavDevice.on('ButtonA_down')
 def ButtonA_down_callback('ButtonA_down', data):
-	''' Go into the "waiting" state '''
-	NavDevice.storage.state = "waiting"
+    ''' Go into the "waiting" state '''
+    NavDevice.storage.state = "waiting"
 
 @NavDevice.on('ButtonB_down')
 def ButtonB_down_callback('ButtonB_down', data):
-	''' Go into manual control mode '''
-	NavDevice.storage.state = "manual"
+    ''' Go into manual control mode '''
+    NavDevice.storage.state = "manual"
 
 @NavDevice.on('ButtonY_down')
 def ButtonY_down_callback('ButtonY_down', pos):
-	''' Add the rover's current position as a waypoint '''
-	NavDevice.storage.waypoints.append(NavDevice.storage.position)
+    ''' Add the rover's current position as a waypoint '''
+    NavDevice.storage.waypoints.append(NavDevice.storage.position)
 
 @NavDevice.on('saveWayPoint')
 def saveWayPoint('saveWayPoint',path):
-	''' Save waypoints to a file in json format '''
-	f = open(path,'w')
-	for waypoint in NavDevice.storage.waypoints:
-		json.dump(waypoint.__dict__,f)
-		f.write('\n')
-	NavDevice.storage.waypoints = []
+    ''' Save waypoints to a file in json format '''
+    f = open(path,'w')
+    for waypoint in NavDevice.storage.waypoints:
+        json.dump(waypoint.__dict__,f)
+        f.write('\n')
+    NavDevice.storage.waypoints = []
 
 @NavDevice.on(loadWayPoint)
 def loadWayPoint_callback('loadWayPoint', path):
-	''' Load waypoints from a json file '''
-	f = open(path,'r')
-	json_data = f.read()
-	data_list = json_data.split('\n')
-	for s in data_list[0:-1]:
-		data = json.loads(s)
-		pos = GPSPosition(data['lat'],data['lon'],data['mode'])
-		NavDevice.storage.waypoints.append(pos)
+    ''' Load waypoints from a json file '''
+    f = open(path,'r')
+    json_data = f.read()
+    data_list = json_data.split('\n')
+    for s in data_list[0:-1]:
+        data = json.loads(s)
+        pos = GPSPosition(data['lat'],data['lon'],data['mode'])
+        NavDevice.storage.waypoints.append(pos)
 
 @NavDevice.on('clearWayPoint')
 def clearWayPoint_callback('clearWayPoint', data):
-	''' Clear all waypoints '''
-	NavDevice.storage.waypoints = []
+    ''' Clear all waypoints '''
+    NavDevice.storage.waypoints = []
 
 @NavDevice.on('autonomousMode')
 def autonomousMode_callback('autonomousMode', flag):
-	''' Enable autonomous navigation mode '''
-	NavDevice.storage.autonomous_mode = flag
+    ''' Enable autonomous navigation mode '''
+    NavDevice.storage.autonomous_mode = flag
 
 NavDevice.run()
