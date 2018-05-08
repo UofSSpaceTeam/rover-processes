8f1cea123b2e2fa943e31f567a57ca01e8d6e110
Carl Hofmeister <hofmeister.carl@gmail.com>
2018-05-08 15:37:02 -0600

    Removed old navigation process

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ process_list = [
     RunOnce('KalmanFilter', 'python3 KalmanFilterProcess.py {}'.format(network)),
     # RunOnce('Autopilot', 'python3 Autopilot.py {}'.format(network)),
     RunOnce('WebUI', 'python3 server.py {}'.format(network), cwd='rover-webui'),
-    RunOnce('Navigation', 'python3 NewNavigationProcess.py {}'.format(network)),
+    RunOnce('Navigation', 'python3 NavigationProcess.py {}'.format(network)),
     # RunOnce('Simulator', 'python3 Simulator.py {}'.format(network)),
 ]
 
