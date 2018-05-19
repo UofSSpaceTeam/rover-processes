8216473c8f8f1b0ca215d6bc8e4b0ff874421c8c
Adam Jones <jones.adam@live.ca>
2018-05-19 14:51:05 -0600

    made triggers behave same as in windows if using linux

--- JoystickProcess.py
+++ JoystickProcess.py
@@ -24,23 +24,35 @@ def buttonAssign():
     if os.name == 'nt':
         rightHoriz = 4
         rightVert = 3
+        trigger = 2
     else:
         rightHoriz = 3
         rightVert = 4
-    return rightHoriz, rightVert
+        trigger = [2,5]
+    return rightHoriz, rightVert, trigger
 
 @JoystickDevice.every(LOOP_PERIOD)
 async def every():
     # assigns os specific joystick assignments
     rightHor = buttonAssign()[0]
     rightVer = buttonAssign()[1]
+    trigger = buttonAssign()[2]
     pygame.event.get()
     left = [JoystickDevice.storage.joystick1.get_axis(0), JoystickDevice.storage.joystick1.get_axis(1)]
     right = [JoystickDevice.storage.joystick1.get_axis(rightHor), JoystickDevice.storage.joystick1.get_axis(rightVer)]
+    # windows treats two triggers as one value, so this code makes linux behave in the same way for the sake of consistency
+    if os.name == 'nt':
+        trig = JoystickDevice.storage.joystick1.get_axis(trigger)
+    else:
+        trig = 1 + JoystickDevice.storage.joystick1.get_axis(trigger[0]) + JoystickDevice.storage.joystick1.get_axis(trigger[1])
+
+
     await JoystickDevice.publish('joystick1', left)
     await JoystickDevice.publish('joystick2', right)
-    # print('left: ' + str(left))
-    # print('right: ' + str(right))
+    await JoystickDevice.publish('trigger', trig)
+    print('left: ' + str(left))
+    print('right: ' + str(right))
+    print('trig: ' + str(trig))
 try:
     JoystickDevice.start()
     JoystickDevice.wait()
