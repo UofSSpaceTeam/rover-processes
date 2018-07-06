25f1155e3e2a4ef64959798fa004a0d8fc874592
tyrelkostyk <tck290@mail.usask.ca>
2018-07-05 20:46:15 -0600

    Finished callback functions, added await statements to publish loaderBody & loaderBucket messages

--- FrontEndLoaderProcess.py
+++ FrontEndLoaderProcess.py
@@ -5,32 +5,37 @@ log = config.logger
 CONTROLLER_NUM = config.front_end_loader_controller
 
 
+# Initialize Device
 FELoaderDevice = Device('FELoaderDevice', 'rover', network=config.network)
 
-# Initialize setup variables
-# ...
 
+# Initialize setup variables
+MAX_DUTY_CYCLE = int(1e5)
+
+
+# Joystick Callback Functions
 @FELoaderDevice.on('*/controller{}/joystick1'.format(CONTROLLER_NUM))
 async def joystick1_callback(joystick1, data):
-    """ Handles the <...> linear actuator for manual control
+    """ Handles the loader's body linear actuator for manual control
             A Joystick message contains:
             [x axis (float -1:1), y axis (float -1:1)]
     """
     y_axis = data[1]
     if y_axis is None:
         return
+
+    await FELoaderDevice.publish('loaderBody', {'SetDutyCycle':int(MAX_DUTY_CYCLE*y_axis)})
+
     
 @FELoaderDevice.on('*/controller{}/joystick2'.format(CONTROLLER_NUM))
 async def joystick2_callback(joystick2, data):
-    """ Handles the <...> linear actuator for manual control
+    """ Handles the loader's bucket linear actuator for manual control
             A Joystick message contains:
             [x axis (float -1:1), y axis (float -1:1)]
     """
     y_axis = data[1]
     if y_axis is None:
         return
-    await FELoaderDevice.publish('
-
     
-### More motors // DoF ?
-### Duty Cycle?
+    await FELoaderDevice.publish('loaderBucket', {'SetDutyCycle':int(MAX_DUTY_CYCLE*y_axis)})
+
