d804623418f974a6fea889968ad892b574bd4bf1
Adam Jones <jones.adam@live.ca>
2018-07-26 22:42:26 -0600

    DrillProcess: fixed DrillDevice name data, removed unecessary variable definitions

--- DrillProcess.py
+++ DrillProcess.py
@@ -11,14 +11,13 @@ TRIG_DEADZONE = 0.05
 controller_num = config.science_controller
 
 #INITIALIZTION
-DrillDevice = Device('ScienceModule', 'rover', network=config.network)
+DrillDevice = Device('DrillDevice', 'rover', network=config.network)
 DrillDevice.storage.top_motor_movement = 'stopped'
 DrillDevice.storage.bottom_motor_movement = 'stopped'
 DrillDevice.storage.rotation_direction = 'stopped'
 
 @DrillDevice.on('*/controller{}/joystick1'.format(controller_num))
 async def set_top_movement(joystick1, data):
-    top_move = DrillDevice.storage.top_motor_movement
     axis = data[1]
 
     if axis is None:
@@ -27,17 +26,16 @@ async def set_top_movement(joystick1, data):
         duty_cycle = axis * DRILL_RAISE_DUTY_CYCLE
 
     if -DEADZONE < axis < DEADZONE:
-        top_move = 'stopped'
+        DrillDevice.storage.top_motor_movement = 'stopped'
         await DrillDevice.publish('DrillBottom', {'SetDutyCycle': int(0)})
     else:
-        top_move = 'moving'
+        DrillDevice.storage.top_motor_movement = 'moving'
         await DrillDevice.publish('DrillTop', {'SetDutyCycle': int(duty_cycle)})
 
-    log.debug('setting top movement to {}'.format(top_move))
+    log.debug('setting top movement to {}'.format(DrillDevice.storage.top_motor_movement))
 
 @DrillDevice.on('*/controller{}/joystick2.'.format(controller_num))
 async def set_bottom_movement(joystick2, data):
-    bot_move = DrillDevice.storage.bottom_motor_movement
     axis = data[1]
 
     if axis is None:
@@ -46,17 +44,16 @@ async def set_bottom_movement(joystick2, data):
         duty_cycle = axis * DRILL_RAISE_DUTY_CYCLE
 
     if -DEADZONE < axis < DEADZONE:
-        bot_move = 'stopped'
+        DrillDevice.storage.bottom_motor_movement = 'stopped'
         await DrillDevice.publish('DrillBottom', {'SetDutyCycle': int(0)})
     else:
-        bot_move = 'moving'
+        DrillDevice.storage.bottom_motor_movement = 'moving'
         await DrillDevice.publish('DrillBottom', {'SetDutyCycle': int(duty_cycle)})
 
-    log.debug('setting bottom movement to {}'.format(bot_move))
+    log.debug('setting bottom movement to {}'.format(DrillDevice.storage.bottom_motor_movement))
 
 @DrillDevice.on('*/controller{}/trigger.'.format(controller_num))
 async def set_drill_rotation(trigger, data):
-    rot_move = DrillDevice.storage.rotation_direction
     axis = data
 
     if axis is None:
@@ -65,20 +62,16 @@ async def set_drill_rotation(trigger, data):
         duty_cycle = axis * ROTATION_SPEED
 
     if -TRIG_DEADZONE < axis < TRIG_DEADZONE:
-        rot_move = 'stopped'
+        DrillDevice.storage.rotation_direction = 'stopped'
         await DrillDevice.publish('DrillSpin', {'SetRPM': int(0)})
     else:
-        rot_move = 'moving'
+        DrillDevice.storage.rotation_direction = 'moving'
         await DrillDevice.publish('DrillSpin', {'SetRPM': int(duty_cycle)})
 
-    log.debug('Setting rotation movement to {}'.format(rot_move))
+    log.debug('Setting rotation movement to {}'.format(DrillDevice.storage.rotation_direction))
 
 @DrillDevice.on('controller{}/{}_down'.format(controller_num, 'buttonB'))
 async def hard_stop():
-
-    top_move = DrillDevice.storage.top_motor_movement
-    bot_move = DrillDevice.storage.bottom_motor_movement
-    rot_move = DrillDevice.storage.rotation_direction
 
     await DrillDevice.publish('DrillTop', {'SetDutyCycle': int(0)})
     await DrillDevice.publish('DrillBottom', {'SetDutyCycle': int(0)})
@@ -88,9 +81,9 @@ async def hard_stop():
     DrillDevice.storage.bottom_motor_movement = 'stopped'
     DrillDevice.storage.rotation_direction = 'stopped'
 
-    log.debug('setting top movement to {}'.format(top_move))
-    log.debug('setting bottom movement to {}'.format(bot_move))
-    log.debug('Setting rotation movement to {}'.format(rot_move))
+    log.debug('setting top movement to {}'.format(DrillDevice.storage.top_motor_movement))
+    log.debug('setting bottom movement to {}'.format(DrillDevice.storage.bottom_motor_movement))
+    log.debug('Setting rotation movement to {}'.format(DrillDevice.storage.rotation_direction))
 
 DrillDevice.start()
 DrillDevice.wait()
