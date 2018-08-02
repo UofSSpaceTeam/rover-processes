a9ab48bcda9070ad374cc78553d3a634c67b9ba3
Lars-Kish <lwk045@mail.usask..ca>
2018-08-01 19:07:50 -0600

    Find max power point

--- YagiSdrProcess.py
+++ YagiSdrProcess.py
@@ -1,6 +1,6 @@
 from rtlsdr import RtlSdr
 import matplotlib.pyplot as plt
-
+import numpy as np
 
 sdr = RtlSdr(serial_number='00000001')
 
@@ -11,12 +11,12 @@ sdr.freq_correction = -4
 sdr.gain = "auto"
 sdr.center_freq = 434e6
 
-
+sdr.set_bandwidth(8000)
 
 samples = sdr.read_samples(256*1024)
 sdr.close()
 
-plt.psd(samples,NFFT=1024,Fs=sdr.sample_rate/1e6,Fc=sdr.center_freq/1e6)
+power,psdFreq = plt.psd(samples,NFFT=1024,Fs=sdr.sample_rate/1e6,Fc=sdr.center_freq/1e6)
 plt.xlabel("freq")
 plt.ylabel("Power")
 
@@ -24,4 +24,14 @@ plt.show()
 #print("getting serial")
 print(samples)
 #print(RtlSdr.get_device_serial_addresses())
+
+powerDb = 10*np.log10(power)
+
+maxPower = max(powerDb)
+maxInd =powerDb.tolist().index(maxPower)
+maxFreq = psdFreq[maxInd]
+
+print(maxPower)
+print(maxFreq)
+
 
